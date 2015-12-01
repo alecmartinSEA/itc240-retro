d8adc136c14e37b3a8782fe9c3d34d1ce55dd171
alecmartinSEA <alecmartin28@gmail.com>
2015-11-30 16:33:44 -0800

    physical path change

--- includes/config.php
+++ includes/config.php
@@ -14,8 +14,8 @@ include 'MyAutoLoader.php'; //loads class that autoloads all classes in include 
 include 'credentials.php';
 
 /* automatic path settings - use the following path settings for placing all code in one application folder */ 
-define('VIRTUAL_PATH', 'http://alecmartin25/retro/'); # Virtual (web) 'root' of application for images, JS & CSS files
-define('PHYSICAL_PATH', '/home/alemar167/alecmartin25.com/retro/'); # Physical (PHP) 'root' of application for file & upload reference
+define('VIRTUAL_PATH', 'http://alecmartin25/itc240/retro/'); # Virtual (web) 'root' of application for images, JS & CSS files
+define('PHYSICAL_PATH', '/home/alemar167/alecmartin25.com/itc240/retro/'); # Physical (PHP) 'root' of application for file & upload reference
 define('INCLUDE_PATH', PHYSICAL_PATH . 'includes/'); # Path to PHP include files - INSIDE APPLICATION ROOT
 
 ob_start();  #buffers our page to be prevent header errors. Call before INC files or ANY html!
